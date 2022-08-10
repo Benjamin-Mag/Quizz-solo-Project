f2ccd8069fb608f08d1c9e3574b9a0b575f7675a
Bellyvokateur <benjamin.mag.p@gmail.com>
2022-08-10 14:30:26 +0200

    adds question/answer template into js

---
+++ script.js
@@ -0,0 +1,28 @@
+// DATA for the Quiz. Array of Objects
+
+const quizQuestions = [
+  {
+    question: "who is the mad hatter ...?",
+    a: "Nobody",
+    b: "everybody",
+    c: "Somebody",
+    d: "not me !",
+    goodOne: "d",
+  },
+  {
+    question: "Is the white Rabbit late ? ",
+    a: "Never",
+    b: "he's always in time",
+    c: "who is he ? ",
+    d: "to my brithday ?",
+    goodOne: "a",
+  },
+  {
+    question: "what's the good answer ?",
+    a: "maybe this one",
+    b: "or this one",
+    c: "ask to the mad hatter",
+    d: "love",
+    goodOne: "d",
+  },
+];
